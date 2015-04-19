cbe610ff6e8b23f0573cd9019a7820caf5bcf5a2
Mercer Traieste <mercer.traieste@gmail.com>
2015-04-19 11:53:37 +0300

    added tools for publishing and creating new presentations

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    GH_PAGES='../devops-training-dist'
+else
+    GH_PAGES=$1
+fi
+
+MASTER="$( cd "$( dirname "$0" )" && pwd )"
+
+cd ${GH_PAGES}
+git rm -rf *
+cp -rf ${MASTER}/* ${GH_PAGES}
+git add -A
+git commit -m "update to new version"
+git push origin gh-pages
